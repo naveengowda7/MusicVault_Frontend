339d3e1c0f71607ffdd16c9320e5786ea9a6ae15
Naveen Gowda MY <naveengowda23277@gmail.com>
2025-03-07 23:01:19 +0530

    added host

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: process.env.PORT || 5173,
     strictPort: true,
-    cors: {
-      origin: '*',
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    },
-  },
-  plugins: [react()],
-})
+    allowedHosts: ['musicvault-frontend.onrender.com'],
+  }
+});
