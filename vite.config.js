e0501c7ac3a61aa9f7c2132627e4cacaf6b2b597
Naveen Gowda MY <naveengowda23277@gmail.com>
2025-03-07 22:58:19 +0530

    added host

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: process.env.PORT || 5173,
+    strictPort: true,
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    },
+  },
   plugins: [react()],
 })
